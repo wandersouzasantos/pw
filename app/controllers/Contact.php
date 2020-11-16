ad39e97819019822391f9e9b331c2f76e92e7820
jonasmsjunior <jonas.uft@gmail.com>
2020-11-15 23:53:27 -0300

    inicio mvc

--- app/controllers/Contact.php
+++ app/controllers/Contact.php
@@ -2,16 +2,22 @@
 
 class Contact extends Controller {
 
-    // Index of the contact page (localhost/contact(/index))
+   
+    // Index of the home page (localhost/home(/index))
     public function index($param1= '', $param2= '', $param3= '') {
         
-        // Initialize Test model
-        $test = $this->model('Test');
+        // Initialize Alunos model
+        $test = $this->model('Alunos');
 
         // Call function from the model
-        $testData = $test->getTestFunction();
+        $testData = $test->getAlunosFunction();
 
-        $this->view('contact/index', ['test' => testData, 'parameters' => [$param1, $param2, $param3]]);
+        $this->view('contact/index', ['alunos' => $testData]);
+    }
+    public function teste($param1= '', $param2= '', $param3= ''){
+        $test = $this->model('Alunos');
+        $alunos = $test->getAlunosForName($param2);
+        $this->view('contact/index', ['alunos' => $alunos]);
     }
 }
 
